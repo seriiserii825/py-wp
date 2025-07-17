5e0a2da39e6c877a5502055e7d6c6e9a2a9fc71d
serii burduja <seriiburduja@gmail.com>
2025-07-17 17:27:16 +0300

    feat: update print

--- classes/utils/Print.py
+++ classes/utils/Print.py
@@ -4,11 +4,15 @@ from rich import print
 class Print:
     @staticmethod
     def info(message):
-        print(f"====== [blue]{message} ======")
+        print("=" * 20)
+        print(f"[blue]{message}")
+        print("=" * 20)
 
     @staticmethod
     def success(message):
+        print("=" * 20)
         print(f"[green]{message}")
+        print("=" * 20)
 
     @staticmethod
     def error(message):
