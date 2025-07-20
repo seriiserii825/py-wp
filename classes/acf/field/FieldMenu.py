e55c355fadb292f315bf60ae15426e5676b0627b
serii <seriiburduja@gmail.com>
2025-07-20 21:42:46 +0300

    upd: field menu move_after_create update

--- classes/acf/field/FieldMenu.py
+++ classes/acf/field/FieldMenu.py
@@ -22,7 +22,19 @@ class FieldMenu:
             if field:
                 field.print_field_with_subfields(index=index, indent=0)
 
-    def move_after_create(self, fields: list, data):
+    def create_field(self):
+        data = self.repo.load()
+        fields = data[0].get("fields", [])
+
+        new_field = self.creator.create()
+        fields.append(new_field)
+
+        self._move_after_create(fields, data)
+
+        self.repo.save(data)
+        print("Field created and saved.")
+
+    def _move_after_create(self, fields: list, data):
         try:
             source_index = str(len(fields) - 1)
             dest = InputValidator.get_string("Enter destination index (e.g. 0.1): ")
@@ -32,25 +44,11 @@ class FieldMenu:
         except Exception as e:
             print(f"Error moving newly created field: {e}")
 
-    def create_field(self):
-        data = self.repo.load()
-        fields = data[0].get("fields", [])
-
-        new_field = self.creator.create()
-        fields.append(new_field)
-
-        self.move_after_create(fields, data)
-
-        self.repo.save(data)
-        print("Field created and saved.")
-
     def move_field(self):
         data = self.repo.load()
         fields = data[0].get("fields", [])
 
-        if not fields:
-            print("No fields to move.")
-            return
+        self._check_field_is_empty(fields)
 
         try:
             source = InputValidator.get_string("Enter source index (e.g. 1.2): ")
@@ -65,9 +63,7 @@ class FieldMenu:
         data = self.repo.load()
         fields = data[0].get("fields", [])
 
-        if not fields:
-            print("No fields to edit.")
-            return
+        self._check_field_is_empty(fields)
 
         try:
             index_path = InputValidator.get_string("Enter field index (e.g. 1.2): ")
@@ -140,9 +136,7 @@ class FieldMenu:
         data = self.repo.load()
         fields = data[0].get("fields", [])
 
-        if not fields:
-            print("No fields to delete.")
-            return
+        self._check_field_is_empty(fields)
 
         try:
             index_path_str = InputValidator.get_string(
@@ -177,9 +171,7 @@ class FieldMenu:
         data = self.repo.load()
         fields = data[0].get("fields", [])
 
-        if not fields:
-            print("No fields to delete.")
-            return
+        self._check_field_is_empty(fields)
 
         try:
             index_path_input = InputValidator.get_string(
@@ -261,3 +253,8 @@ class FieldMenu:
             f"Enter new value for '{selected_attr}': "
         )
         return new_value.strip()
+
+    def _check_field_is_empty(self, fields: list):
+        if not fields:
+            print("No fields to move.")
+            return
