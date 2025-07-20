966a4dfd0470c024715e4d5aac1450e6175894fa
serii <seriiburduja@gmail.com>
2025-07-20 15:06:41 +0300

    feat: acf menu

--- acf/field/field_menu.py
+++ acf/field/field_menu.py
@@ -1,26 +1,41 @@
 from classes.acf.AcfTransfer import AcfTransfer
 from classes.acf.field.FieldMenu import FieldMenu
+from classes.utils.Menu import Menu
 
 
 def field_menu(section_file_json_path):
+    while True:
+        acf_menu(section_file_json_path)
+
+
+def acf_menu(section_file_json_path):
     f_menu = FieldMenu(section_file_json_path)
     f_menu.show_all()
-    # f_menu.create_field()
-    f_menu.move_field()
-    f_menu.show_all()
-    to_upload = input("Do you want to upload the changes? (y/n): ").strip().lower()
-    if to_upload == "y":
-        AcfTransfer.wp_import()
-        print("Changes uploaded successfully.")
+    menu_headers = ["Index", "Option"]
+    menu_rows = [["0", "Create new field"], ["1", "Move field"], ["2", "Exit"]]
+    Menu.display("Welcome to ACF Field Menu", menu_headers, menu_rows)
+    choice = Menu.choose_option()
+    if choice == 0:
+        f_menu.create_field()
+        upload_changes()
+    elif choice == 1:
+        f_menu.move_field()
+        upload_changes()
+    elif choice == 2:
+        print("Exiting ACF Field Menu.")
+        return
     else:
-        print("Changes not uploaded.")
-    # while True:
-    #     choice = input("Do you want to create another field? (y/n): ").strip().lower()
-    #     if choice == "y":
-    #         f_menu.create_field()
-    #         f_menu.show_all()
-    #     elif choice == "n":
-    #         print("Exiting field menu.")
-    #         break
-    #     else:
-    #         print("Invalid choice. Please enter 'y' or 'n'.")
+        print("Invalid choice. Please try again.")
+        return
+
+
+def upload_changes():
+    upload = (
+        input("Do you want to upload changes to WordPress? (y/n): ").strip().lower()
+    )
+    if upload == "y":
+        print("Uploading changes to WordPress...")
+        AcfTransfer.wp_import()
+    else:
+        print("Changes not uploaded to WordPress.")
+        return
