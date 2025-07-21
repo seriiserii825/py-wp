3178f681d92e123feb89c8bfa5f7802b953e4407
serii burduja <seriiburduja@gmail.com>
2025-07-21 16:28:39 +0300

    feat: upload changes in main menu

--- acf/field/field_menu.py
+++ acf/field/field_menu.py
@@ -19,34 +19,33 @@ def acf_menu(section_file_json_path):
         ["3", "Delete field"],
         ["4", "Delete multiple fields"],
         ["5", "Copy group to clipboard"],
-        ["6", "Exit"],
+        ["6", "Upload changes to WordPress"],
+        ["7", "Exit"],
     ]
     Menu.display("Welcome to ACF Field Menu", menu_headers, menu_rows)
     choice = Menu.choose_option()
     if choice == 0:
         f_menu.create_field()
         f_menu.show_all()
-        upload_changes()
     elif choice == 1:
         f_menu.move_field()
         f_menu.show_all()
-        upload_changes()
     elif choice == 2:
         f_menu.edit_field()
         f_menu.show_all()
-        upload_changes()
     elif choice == 3:
         f_menu.delete_field()
         f_menu.show_all()
-        upload_changes()
     elif choice == 4:
         f_menu.delete_fields()
         f_menu.show_all()
-        upload_changes()
     elif choice == 5:
         f_menu.show_all()
         f_menu.copy_group_to_clipboard()
     elif choice == 6:
+        upload_changes()
+        f_menu.show_all()
+    elif choice == 7:
         print("Exiting ACF Field Menu.")
         exit()
     else:
