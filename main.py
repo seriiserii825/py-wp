08df1c74a32d36b3193c54166da2fbf3f0dd2ccd
serii burduja <seriiburduja@gmail.com>
2025-07-25 09:57:52 +0300

    feat: 0.2.0 version - plugin managment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from classes.utils.Menu import Menu
 from classes.utils.WPPaths import WPPaths
 from main_menu.acf_func import acf_func
 from main_menu.backup_menu import backup_menu
@@ -8,7 +9,26 @@ from modules.check_is_wp import check_is_wp
 
 
 def menu():
-    acf_func()
+    headers = ["Index", "Option"]
+    row_styles = {
+        0: "bold blue",
+        1: "bold green",
+    }
+    rows = [
+        ["0", "ACF"],
+        ["1", "Plugins"],
+    ]
+    Menu.display("Main Menu", headers, rows, row_styles=row_styles)
+    choice = Menu.choose_option()
+    if choice == 0:
+        acf_func()
+        menu()
+    elif choice == 1:
+        plugins_menu()
+        menu()
+    else:
+        print("Invalid choice. Please try again.")
+        menu()
     # backup_menu()
     # plugins_menu()
 
