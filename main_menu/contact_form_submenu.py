14a7c18e107682c237db25828904741cc46ecc32
serii <seriiburduja@gmail.com>
2025-08-07 20:18:31 +0300

    upd: refactor submenu

--- main_menu/contact_form_submenu.py
+++ main_menu/contact_form_submenu.py
@@ -5,35 +5,26 @@ from rich import print
 def contact_form_submenu(
     form_files_paths, all_fields, required_fields, submited_fields
 ):
-    print("[blue]1. Show contact form fields")
-    print("[green]2. Show files")
-    print("[blue]3. Show random_fields")
-    print("[red]4. Exit")
+    while True:
+        print("\n[bold cyan]Contact Form Menu")
+        print("[blue]1. Show contact form fields")
+        print("[green]2. Show files")
+        print("[blue]3. Show random fields")
+        print("[red]4. Exit")
 
-    option = input("Select an option: ")
-    if option == "1":
-        ContactForm.show_contact_form_fields(
-            all_fields, required_fields, submited_fields
-        )
-        contact_form_submenu(
-            form_files_paths, all_fields, required_fields, submited_fields
-        )
-    elif option == "2":
-        ContactForm.show_contact_form_files(form_files_paths)
-        contact_form_submenu(
-            form_files_paths,
-            all_fields,
-            required_fields,
-            submited_fields,
-        )
-    elif option == "3":
-        ContactForm.show_random_fields()
-        contact_form_submenu(
-            form_files_paths,
-            all_fields,
-            required_fields,
-            submited_fields,
-        )
-    else:
-        print("[red]Invalid option")
-        exit()
+        option = input("Select an option: ").strip()
+
+        match option:
+            case "1":
+                ContactForm.show_contact_form_fields(
+                    all_fields, required_fields, submited_fields
+                )
+            case "2":
+                ContactForm.show_contact_form_files(form_files_paths)
+            case "3":
+                ContactForm.show_random_fields()
+            case "4":
+                print("[bold red]Exiting menu...")
+                break
+            case _:
+                print("[red]Invalid option. Please try again.")
