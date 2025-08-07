199c474f44931d59ec897045d008a766d3e6e774
serii <seriiburduja@gmail.com>
2025-08-07 14:58:46 +0300

    feat: check wp version for local wp started

--- modules/check_is_wp.py
+++ modules/check_is_wp.py
@@ -1,5 +1,6 @@
 import os
 
+from classes.utils.Command import Command
 from classes.utils.Print import Print
 
 
@@ -9,4 +10,10 @@ def check_is_wp():
             "This is not a WordPress project."
             " Please run this command in the root of your WordPress project."
         )
+        exit(1)
+
+    try:
+        Command.run("wp core is-installed")
+    except RuntimeError as e:
+        Print.error(f"Error: {e}")
         exit(1)
