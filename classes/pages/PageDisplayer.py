87bd013f35479e579ee4d9ec3944100a2c29d686
serii burduja <seriiburduja@gmail.com>
2026-08-07 13:14:27 +0300

    feat: display slug in list pages

--- classes/pages/PageDisplayer.py
+++ classes/pages/PageDisplayer.py
@@ -9,11 +9,12 @@ class PageDisplayer:
         templates = PageManager.get_templates()
         names_by_file = {file: name for name, file in templates.items()}
 
-        headers = ["ID", "Title", "Status", "Template", "Date"]
+        headers = ["ID", "Title", "Slug", "Status", "Template", "Date"]
         data = [
             [
                 str(p.ID),
                 p.post_title,
+                p.post_name,
                 p.post_status,
                 names_by_file.get(p.page_template, p.page_template),
                 p.post_date,
