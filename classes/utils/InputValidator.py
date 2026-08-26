c43aea5bc23ae5e68e2461ffd1aea2e042db33ef
serii burduja <seriiburduja@gmail.com>
2026-08-26 10:30:27 +0300

    feat: input validator colors

--- classes/utils/InputValidator.py
+++ classes/utils/InputValidator.py
@@ -1,65 +1,67 @@
-from rich import print
+from rich.console import Console
+
+console = Console()
 
 
 class InputValidator:
     def _pretty_print(self, value):
-        print("[green]============================")
-        print(f"Value: {value}")
-        print("[green]============================")
+        console.print("[green]============================")
+        console.print(f"Value: {value}")
+        console.print("[green]============================")
 
     @staticmethod
     def get_int(prompt="Enter an integer: "):
         while True:
             try:
-                return int(input(prompt))
+                return int(console.input(prompt))
             except ValueError:
-                print("Invalid input. Please enter a valid integer.")
+                console.print("Invalid input. Please enter a valid integer.")
 
     @staticmethod
     def get_float(prompt="Enter a number: "):
         while True:
             try:
-                return float(input(prompt))
+                return float(console.input(prompt))
             except ValueError:
-                print("Invalid input. Please enter a valid number.")
+                console.print("Invalid input. Please enter a valid number.")
 
     @staticmethod
     def get_positive_int(prompt="Enter a number: "):
         while True:
-            value = input(prompt).strip()
+            value = console.input(prompt).strip()
             try:
                 num = int(value)
             except ValueError:
-                print("Invalid input. Please enter a valid number!")
+                console.print("Invalid input. Please enter a valid number!")
                 continue
             if num <= 0:
-                print("Invalid input. Please enter a number greater than 0!")
+                console.print("Invalid input. Please enter a number greater than 0!")
                 continue
             return num
 
     @staticmethod
     def get_string(prompt="Enter text: ", allow_empty=False):
         while True:
-            value = input(prompt)
+            value = console.input(prompt)
             value = value.strip()
             if value or allow_empty:
                 return value
-            print("Input cannot be empty. Try again.")
+            console.print("Input cannot be empty. Try again.")
 
     @staticmethod
     def get_bool(prompt="Enter 'y' for yes or 'n' for no: "):
         while True:
-            value = input(prompt).strip().lower()
+            value = console.input(prompt).strip().lower()
             if value in ("y", "yes"):
                 return True
             elif value in ("n", "no"):
                 return False
-            print("Invalid input. Please enter 'y' or 'n'.")
+            console.print("Invalid input. Please enter 'y' or 'n'.")
 
     @staticmethod
     def get_bool_true_default(prompt="Enter 'n' for no or 'y' by default: "):
         while True:
-            value = input(prompt).strip().lower()
+            value = console.input(prompt).strip().lower()
             if value in ("n", "no"):
                 return False
             return True
